53bd0381827759e922c0ae5f1dae6919df234278
Alexander Letnikow <reclamme@gmail.com>
2017-10-03 10:13:12 +0300

    Implement CalculatorSlider (using rc-slider)

---
+++ src/CalculatorSlider/CalculatorSlider.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import * as PropTypes from "prop-types";
+import {
+    CalculatorControlWrapper, CalculatorControlWrapperContext,
+    CalculatorControlWrapperContextTypes
+} from "..";
+import Slider from "rc-slider";
+
+export class CalculatorSlider extends React.Component {
+    public static readonly contextTypes = CalculatorControlWrapperContextTypes;
+
+    public context: CalculatorControlWrapperContext;
+
+    public render(): JSX.Element {
+        return (
+            <Slider
+                value={this.context.calculationValue}
+                min={this.context.Conditions.min}
+                max={this.context.Conditions.max}
+                step={this.context.Conditions.step}
+                onChange={this.context.onCalculationChange}
+            />
+        );
+    }
+}
